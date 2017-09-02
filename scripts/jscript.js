576a6b3f7ef32efbb5cde420440067385038d68b
bayungPradip <31308437+bayungPradip@users.noreply.github.com>
2017-09-02 12:11:07 +0545

    new1

--- scripts/jscript.js
+++ scripts/jscript.js
@@ -6,6 +6,7 @@ function onclicked()
 				{
 					document.getElementById('img1').src="resource/light-bulb-off-icon.png";
 					document.getElementById('img2').src="resource/switch-off-icon.png";
+					document.getElementById('body').style.backgroundColor="white";
 					
 
 				}
@@ -13,6 +14,7 @@ function onclicked()
 				{
 					document.getElementById("img1").src="resource/light-bulb-on-icon.png";
 					document.getElementById('img2').src="resource/switch-on-icon.png";
+					document.getElementById('body').style.backgroundColor="grey";
 
 				}
 	}
