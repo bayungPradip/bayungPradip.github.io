0c258f7e4046331fe297c95c63b1aac7a74423ec
bayungPradip <31308437+bayungPradip@users.noreply.github.com>
2017-09-02 12:01:32 +0545

    new

---
+++ scripts/jscript.js
@@ -0,0 +1,20 @@
+var i=0;
+function onclicked()
+	{
+			i++;
+			if((i%2)==0)
+				{
+					document.getElementById('img1').src="resource/light-bulb-off-icon.png";
+					document.getElementById('img2').src="resource/switch-off-icon.png";
+					
+
+				}
+			else
+				{
+					document.getElementById("img1").src="resource/light-bulb-on-icon.png";
+					document.getElementById('img2').src="resource/switch-on-icon.png";
+
+				}
+	}
+	
+	
