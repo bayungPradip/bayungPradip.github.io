4891080d43b419a86c8f68c8a28263505e02b1c4
bayungPradip <31308437+bayungPradip@users.noreply.github.com>
2017-09-02 12:36:21 +0545

    latest

--- scripts/jscript.js
+++ scripts/jscript.js
@@ -6,7 +6,7 @@ function onclicked()
 				{
 					document.getElementById('img1').src="resource/light-bulb-off-icon.png";
 					document.getElementById('img2').src="resource/switch-off-icon.png";
-					document.getElementById('body').style.backgroundColor="white";
+					document.getElementById('change').innerHTML="Off";
 					
 
 				}
@@ -14,8 +14,7 @@ function onclicked()
 				{
 					document.getElementById("img1").src="resource/light-bulb-on-icon.png";
 					document.getElementById('img2').src="resource/switch-on-icon.png";
-					document.getElementById('body').style.backgroundColor="grey";
-
+					document.getElementById('change').innerHTML="On";
 				}
 	}
 	
